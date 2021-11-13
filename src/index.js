b1c045ef0c9e78cd9cad0d8b8e6146f612170f24
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-11-14 00:05:23 +0400

    init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import Phaser from "phaser";
 
 const config = {
@@ -6,23 +5,34 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    default: "arcade",
   },
   scene: {
-    preload: preload,
-    create: create
-  }
+    preload,
+    create,
+    update,
+  },
 };
 
+let bird = null;
+
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
+}
+
+function create() {
+  this.add.image(0, 0, "sky").setOrigin(0);
+
+  // if we want bird to have body we add it to physics obj.
+  bird = this.physics.add
+    .sprite(config.width * 0.1, config.height / 2, "bird")
+    .setOrigin(0);
+  bird.body.velocity.y = 100;
+}
+
+function update() {
+  console.log(bird.body.velocity.y);
+}
+
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
