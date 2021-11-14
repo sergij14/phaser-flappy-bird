aaef3c262910b36913d7d6b353fc21886326bb58
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-11-14 10:05:11 +0400

    pipes refactor

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ const config = {
 };
 
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
+
 const pipeVerticalDistaneRange = [150, 250];
 let pipeHorizontalDistance = 0;
 
@@ -42,30 +41,39 @@ function create() {
   bird.body.gravity.y = 200;
 
   for (let i = 0; i < PIPES_NUM; i++) {
-    pipeHorizontalDistance += 400;
-    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistaneRange);
-    let pipeVerticalPosition = Phaser.Math.Between(
-      0 + 20,
-      config.height - 20 - pipeVerticalDistance
-    );
-    upperPipe = this.physics.add
-      .sprite(pipeHorizontalDistance, pipeVerticalPosition, "pipe")
-      .setOrigin(0, 1);
-    lowerPipe = this.physics.add
-      .sprite(
-        pipeHorizontalDistance,
-        upperPipe.y + pipeVerticalDistance,
-        "pipe"
-      )
-      .setOrigin(0, 0);
-    upperPipe.body.velocity.x = -200;
-    lowerPipe.body.velocity.x = -200;
+    const upperPipe = this.physics.add.sprite(0, 0, "pipe").setOrigin(0, 1);
+    const lowerPipe = this.physics.add.sprite(0, 0, "pipe").setOrigin(0, 0);
+
+    placePipe(upperPipe, lowerPipe);
   }
 
   const spaceKey = this.input.keyboard.addKey("SPACE");
   this.input.on("pointerdown", flap);
   spaceKey.on("down", flap);
   console.log(bird);
+}
+
+function update(time, delta) {
+  if (bird.y >= config.height - bird.height || bird.y <= 0)
+    restartPlayerPosition();
+}
+
+function placePipe(uPipe, lPipe) {
+  pipeHorizontalDistance += 400;
+  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistaneRange);
+  let pipeVerticalPosition = Phaser.Math.Between(
+    0 + 20,
+    config.height - 20 - pipeVerticalDistance
+  );
+
+  uPipe.x = pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+
+  uPipe.body.velocity.x = -200;
+  lPipe.body.velocity.x = -200;
 }
 
 function flap() {
@@ -76,11 +84,6 @@ function restartPlayerPosition() {
   bird.y = config.height / 2;
   bird.x = config.width * 0.1;
   bird.body.velocity.y = 0;
-}
-
-function update(time, delta) {
-  if (bird.y >= config.height - bird.height || bird.y <= 0)
-    restartPlayerPosition();
 }
 
 new Phaser.Game(config);
