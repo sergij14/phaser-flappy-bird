65b29e52d9de8b7a17b1740420ac9082ff5582cb
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-11-14 09:57:29 +0400

    palced pipes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import Phaser from "phaser";
 
-let bird = null;
-
-const VELOCITY = 400;
-const flapVelocity = 250;
-
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -13,9 +8,6 @@ const config = {
     default: "arcade",
     arcade: {
       debug: true,
-      gravity: {
-        y: VELOCITY,
-      },
     },
   },
   scene: {
@@ -25,9 +17,20 @@ const config = {
   },
 };
 
+let bird = null;
+let upperPipe = null;
+let lowerPipe = null;
+const pipeVerticalDistaneRange = [150, 250];
+let pipeHorizontalDistance = 0;
+
+const PIPES_NUM = 4;
+const flapVelocity = 200;
+
 function preload() {
   this.load.image("sky", "assets/sky.png");
   this.load.image("bird", "assets/bird.png");
+
+  this.load.image("pipe", "assets/pipe.png");
 }
 
 function create() {
@@ -36,6 +39,28 @@ function create() {
   bird = this.physics.add
     .sprite(config.width * 0.1, config.height / 2, "bird")
     .setOrigin(0);
+  bird.body.gravity.y = 200;
+
+  for (let i = 0; i < PIPES_NUM; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistaneRange);
+    let pipeVerticalPosition = Phaser.Math.Between(
+      0 + 20,
+      config.height - 20 - pipeVerticalDistance
+    );
+    upperPipe = this.physics.add
+      .sprite(pipeHorizontalDistance, pipeVerticalPosition, "pipe")
+      .setOrigin(0, 1);
+    lowerPipe = this.physics.add
+      .sprite(
+        pipeHorizontalDistance,
+        upperPipe.y + pipeVerticalDistance,
+        "pipe"
+      )
+      .setOrigin(0, 0);
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
 
   const spaceKey = this.input.keyboard.addKey("SPACE");
   this.input.on("pointerdown", flap);
